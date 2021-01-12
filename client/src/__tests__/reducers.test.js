8050a753cccb99675105188730450e562e076646
Sarah Gambles <sarahgambles@Sarahs-MacBook-Pro.local>
2021-01-11 17:41:42 -0700

    added details to home an detail pages

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -6,6 +6,7 @@ import {
 } from '../utils/actions';
 
 import { reducer } from '../utils/reducers';
+import { UPDATE_CURRENT_CATEGORY } from '../../../server/utils/actions';
 
 // create a sample of what our global state will look like
 const initialState = {
@@ -24,3 +25,22 @@ test('UPDATE_PRODUCTS', () => {
     expect(initialState.products.length).toBe(0);
 });
 
+test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CATEGORIES,
+        categories: [{}, {}]
+    });
+
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+});
+
+test ('UPDATE_CURRENT_CATEGORY', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CURRENT_CATEGORY,
+        currentCategory: '2'
+    });
+
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
+});
